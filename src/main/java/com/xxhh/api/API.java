43dcb13ca9d80fae226496eee0b3e7e81f460e90
wangzhonghao <wangzhonghao@mobike.com>
2018-04-28 16:30:18 +0800

    add tomcat

--- src/main/java/com/xxhh/api/API.java
+++ src/main/java/com/xxhh/api/API.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoCo
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -17,7 +19,14 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
     DataSourceTransactionManagerAutoConfiguration.class,
     ValidationAutoConfiguration.class, RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
-public class API {
+public class API extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 设置启动类,用于独立tomcat运行的入口
+        return builder.sources(API.class);
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(API.class, args);
     }
