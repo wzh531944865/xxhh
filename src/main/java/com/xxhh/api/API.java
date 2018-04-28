ae4a8eaab9089866ebb0201ba3c2341c6c65cc8a
wangzhonghao <wangzhonghao@mobike.com>
2018-04-28 18:21:05 +0800

    have a try

--- src/main/java/com/xxhh/api/API.java
+++ src/main/java/com/xxhh/api/API.java
@@ -19,14 +19,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
     DataSourceTransactionManagerAutoConfiguration.class,
     ValidationAutoConfiguration.class, RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
-public class API extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // 设置启动类,用于独立tomcat运行的入口
-        return builder.sources(API.class);
-    }
-
-
+public class API  {
     public static void main(String[] args) {
         SpringApplication.run(API.class, args);
     }
